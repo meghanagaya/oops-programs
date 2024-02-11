795da5035e5b9fb8fd48cee17d7958adc5d41505
meghanagaya <158757303+meghanagaya@users.noreply.github.com>
2024-02-11 15:52:23 +0530

    Add files via upload

---
+++ EvenOdd.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num;
+    cout<<"Enter a positive integer : ";
+    cin>>num;
+    if(num==0)
+    {
+        cout<<"The given number "<<num<<" is neither even nor odd"<<endl;
+    }
+    else if(num>0)
+    {
+        if(num%2==0)
+        {
+            cout<<"The given number "<<num<<" is an even number"<<endl;
+        }
+        else
+        {
+            cout<<"The given number "<<num<<" is an odd number"<<endl;
+        }
+    }
+    else
+    {
+        cout<<"Please enter a positive number!"<<endl;
+    }
+}
+
