83e1ef6a5e6c5c239792fb80d44d7e4d74dfd3e9
meghanagaya <158757303+meghanagaya@users.noreply.github.com>
2024-02-25 11:41:06 +0530

    Add files via upload

---
+++ Constructor.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+class Student
+{
+  public
+    Student()
+    {
+      String collegeName="MVGR";
+      int collegeCode=33;
+      System.out.print("CollegeName : "+collegeName+"\n"+"CollegeCode : "+collegeCode+"\n");
+    }
+    Student(String fullName,double semPercentage)
+    {
+      System.out.print("FullName : "+fullName+"\n"+"SemPercentage : "+semPercentage);
+    }
+}
+
+public class Constructor
+{
+  public static void main(String[] args)
+  {
+    @SuppressWarnings("unused")
+    Student meghana=new Student();
+    Scanner input=new Scanner(System.in);
+    System.out.print("FullName : ");
+    String fullName=input.nextLine();
+    System.out.print("SemPercentage : ");
+    double semPercentage=input.nextDouble();
+    input.close();
+    @SuppressWarnings("unused")
+    Student meghana01=new Student(fullName,semPercentage);
+
+  }
+}
