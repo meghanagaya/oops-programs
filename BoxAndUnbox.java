6b022ed83279651e94d4866187d2cbae19ad5fc4
meghanagaya <158757303+meghanagaya@users.noreply.github.com>
2024-04-08 07:01:18 +0530

    Add files via upload

---
+++ BoxAndUnbox.java
@@ -0,0 +1,12 @@
+public class BoxAndUnbox {
+    public static void main(String[] args)
+    {
+    //Boxing
+    Integer a=new Integer(53);
+    Integer b=new Integer(65);
+    //UnBoxing
+    int c=b;
+    System.out.println("Boxed Value : "+a);
+    System.out.println("UnBoxed Value : "+c);
+    }
+}
