9270b53614cdaf1c712d9e3d8c391a17a3e46d0d
meghanagaya <158757303+meghanagaya@users.noreply.github.com>
2024-04-24 21:40:23 +0530

    Add files via upload

---
+++ FileStream.java
@@ -0,0 +1,28 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class FileStream {
+    public static void main(String[] args) {
+        FileInputStream fin=null;
+        FileOutputStream fout=null;
+        try{
+            fin=new FileInputStream("/home/meghana/Documents/Programs/fin.txt");
+            fout=new FileOutputStream("/home/meghana/Documents/Programs/fout.txt");
+            try {
+                System.out.println("Total Number of Bytes : "+fin.available());
+                int i;
+                while ((i=fin.read())!=-1) {
+                    fout.write(i);
+                }
+                fin.close();
+                fout.close();
+            } catch (IOException e) {
+                System.out.println("An error occured while performing input/.");
+            }
+        }catch(FileNotFoundException e){
+            System.out.println("The File is not found here");
+        }
+    }
+}
